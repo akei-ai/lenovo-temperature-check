f0e80b165fd3c3dbf5a6c80f990434d6202b0700
akei-ai <wizmoz41@gmail.com>
2023-11-16 20:27:49 +0800

    changed args

--- temperaturecheck.sh
+++ temperaturecheck.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+    if [[ -z "$1" ]]; then
+        echo "Empty Parameter for Temperature, Defaulting to 80C "
+        TEMP_TARGET=80
+    else
+        TEMP_TARGET=$2
+    fi
 
-     if [[ -z "$1" ]]; then
-        echo "Empty Parameter, Defaulting to 1 second checks"
+    if [[ -z "$2" ]]; then
+        echo "Empty Parameter for Seconds, Defaulting to 1 second checks"
         SLEEP=1
     else
-        SLEEP=$1
+        SLEEP=$2
     fi
-TEMP_TARGET=$2
+
 echo "Starting the Program, will do checks in 5 seconds"
 sleep 5
 while true; do
@@ -15,7 +21,7 @@ while true; do
     echo Lenovo Extreme Cooling
 
     if (( $(echo "$TEMP >= $TEMP_TARGET" | bc -l) )); then
-        ec4Linux enable
+        ec4Linux enable > /dev/null
         echo Enabling ExtremeCooling4Linux
     else
         ec4Linux disable
